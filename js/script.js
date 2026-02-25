5dfdd7fa6d1ee5546f88c63d29af4d827094f2e5
modou niane <mniane2005@gmail.com>
2026-02-25 22:29:25 +0000

    ajout de tous les fichiers du projet

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let content=document.querySelector(".en-tete");
+content.textContent="AT_TIDIANY BIJOUTERIE";
+let collection = document.getElementById("collectionImages");
+collection.innerHTML="<img src='../images/montre/m1.jpeg'><img src='../images/montre/m2.jpeg'><img src='../images/montre/m3.jpeg'><img src='../images/montre/m4.jpeg'><img src='../images/montre/m5.jpeg'><img src='../images/montre/m6.jpeg'><img src='../images/montre/m7.jpeg'>";
