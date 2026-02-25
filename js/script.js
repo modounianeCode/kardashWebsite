8b49bee6bf939b2ecf28abd6990751cd20d49c03
modou niane <mniane2005@gmail.com>
2026-02-25 22:35:25 +0000

    feat:alert dans le site

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ let content=document.querySelector(".en-tete");
 content.textContent="AT_TIDIANY BIJOUTERIE";
 let collection = document.getElementById("collectionImages");
 collection.innerHTML="<img src='../images/montre/m1.jpeg'><img src='../images/montre/m2.jpeg'><img src='../images/montre/m3.jpeg'><img src='../images/montre/m4.jpeg'><img src='../images/montre/m5.jpeg'><img src='../images/montre/m6.jpeg'><img src='../images/montre/m7.jpeg'>";
+
+
+
+
+alert("Bienvenue sur notre site de bijouterie en ligne! Découvrez notre collection de montres élégantes et raffinées. Nous sommes ravis de vous offrir une expérience d'achat exceptionnelle. Explorez notre sélection de montres de qualité supérieure, conçues pour allier style et fonctionnalité. Que vous recherchiez une montre classique ou moderne, nous avons ce qu'il vous faut. Profitez de nos offres exclusives et trouvez la montre parfaite pour compléter votre look. Merci de choisir AT_TIDIANY BIJOUTERIE pour vos besoins en matière de montres!");
